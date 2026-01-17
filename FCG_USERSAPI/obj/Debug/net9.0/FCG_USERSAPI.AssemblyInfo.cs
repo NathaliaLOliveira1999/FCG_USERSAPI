e359d546bf6317519509ccab34dab8a630deda74
natha <natha@NATHALIALO>
2026-01-17 16:00:22 -0300

    feat: first

--- FCG_USERSAPI/obj/Debug/net9.0/FCG_USERSAPI.AssemblyInfo.cs
+++ FCG_USERSAPI/obj/Debug/net9.0/FCG_USERSAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCG_USERSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec456d178c7266eb6d0b2c1fc2e992dc46e6e290")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCG_USERSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCG_USERSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
